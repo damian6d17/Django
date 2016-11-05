abb0fabebf305c76b079bd2c7d12ab5355d79c33
damian6d17 <damian6d17@wp.pl>
2016-11-05 21:03:04 +0100

    poprawka błędów.

--- DjangoProject/dummy_images/views.py
+++ DjangoProject/dummy_images/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from dummy_images.models import DummyImage
-from django.http import HttpResponseNotFound
 
 def cos(request):
 	return render(request, "cos.html", {'cos' : DummyImage.objects.all().order_by('?')[:1] })
